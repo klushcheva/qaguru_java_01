5c313127b0fcf9e79cd3ebe5e313d0417cd49cba
k-lusheva <k.lusheva@vk.team>
2023-12-26 12:23:33 +0300

    fix

--- src/test/java/guru/qa/RegistrationTest.java
+++ src/test/java/guru/qa/RegistrationTest.java
@@ -11,7 +11,7 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class RegistrationTest {
     @BeforeAll
-    static void SetUpBrowser(){
+    static void setUpBrowser(){
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
